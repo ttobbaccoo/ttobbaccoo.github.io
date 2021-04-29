359f6ef930196e2aa4dc647179fd1b4776e3cb10
ttobbaccoo <1511295@mail.nankai.edu.cn>
2021-04-29 03:23:15 -0700

    add citation; update portrait and icon

---
+++ assets/js/cite.js
@@ -0,0 +1,19 @@
+$(cite_bar_emnlp2020).click(function () {
+	var div = document.getElementById("cite_emnlp2020");
+	if (div.style.display == 'block') {
+		div.style.display = 'none';
+	}
+	else {
+		div.style.display = 'block';
+	}
+});
+
+$(cite_bar_aiide2020).click(function () {
+	var div = document.getElementById("cite_aiide2020");
+	if (div.style.display == 'block') {
+		div.style.display = 'none';
+	}
+	else {
+		div.style.display = 'block';
+	}
+});
